f9a6d1434db5f8f79d24484504681742c451c40d
diwangs <27280516+diwangs@users.noreply.github.com>
2018-04-08 13:38:34 +0700

    Added JSON production

--- TweetGrabber.php
+++ TweetGrabber.php
@@ -1,4 +1,5 @@
 <?php 
+    // Init
     require_once('TwitterAPIExchange.php');
     $settings = array(
         'oauth_access_token' => "308341308-pOqDjwHk7l3EFxhnBzyqKUvEt5WTysVT1sC5CZOz",
@@ -10,5 +11,14 @@
     $requestMethod = 'GET';
     $getfields ='?q=xkcd'; // Masukkan query di sini
     $twitter = new TwitterAPIExchange($settings);
-    echo $twitter->setGetfield($getfields)->buildOauth($url, $requestMethod)->performRequest();
+    $tweets_raw = json_decode($twitter->setGetfield($getfields)->buildOauth($url, $requestMethod)->performRequest(), true)['statuses'];
+    echo $tweets_raw[7]['text'];
+    // Jadiin JSON
+    $i = 0;
+    $file = fopen('tweets.json', w);
+    foreach($tweets_raw as $tweet_raw) {
+        $tweets[$i++] = $tweet_raw['text'];
+    }
+    fwrite($file, json_encode($tweets));
+    $fclose($file);
 ?>
