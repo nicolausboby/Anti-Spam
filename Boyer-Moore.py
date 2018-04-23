1fd93a5f9334cec2c4e6e36c4f87c6a1d3c41963
Nicolaus Boby <nicolausboby@gmail.com>
2018-04-23 09:45:59 +0700

    update
    return boolean

--- Boyer-Moore.py
+++ Boyer-Moore.py
@@ -38,9 +38,10 @@ def main():
         pattern = sys.argv[1]
         found = boyer_moore_matching(sample_string, pattern)
         if found == -1:
-            print("no match found!")
+            return false;
         else:
-            print("found match on : ", found, "-", found + len(pattern) - 1)
+            return true;
+            # print("found match on : ", found, "-", found + len(pattern) - 1)
     except IndexError:
         print("Missing Argument(s)!")
 
